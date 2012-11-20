2a65ee212ea3f7f389e97c91a8feb82fac90b55b
Andy White <greatwhitehope@gmail.com>
2012-11-19 23:49:57 -0700

    Move file back

---
+++ js/routers/appRouter.js
@@ -0,0 +1,33 @@
+/**
+ * Returns the main Backbone.Marionette.AppRouter instance
+ */
+define(['jquery', 'underscore', 'backbone', 'marionette', 'js/controllers/appController'],
+
+    function($, _, Backbone, Marionette, appController) {
+
+        console.log('Entering js/routers/appRouter');
+
+        var AppRouter = Marionette.AppRouter.extend({
+
+            initialize: function() {
+                _.bindAll(this);
+            },
+
+            appRoutes: {
+                '': 'index',
+                'login': 'login',
+                'about': 'about',
+                'boxes': 'boxes',
+                '*other': 'other'
+            },
+
+            triggerRoute: function(route) {
+                this.navigate(route, { trigger: true });
+            }
+        });
+
+        return new AppRouter({
+            controller: appController
+        });
+    }
+);
