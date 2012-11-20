538f5a3a05f36732af4d508306b45622b4bc0a87
Andy White <greatwhitehope@gmail.com>
2012-11-20 15:20:51 -0700

    Build cleanup, attempting to get r.js working

---
+++ build/build.require.js
@@ -0,0 +1,14 @@
+// See https://github.com/jrburke/r.js/blob/master/build/example.build.js
+// All paths are relative to the location of this file
+({
+    baseUrl: "../",
+
+    dir: "../intermediate",
+
+    modules: [
+        {
+            name: "config"
+        }
+    ]
+
+})
