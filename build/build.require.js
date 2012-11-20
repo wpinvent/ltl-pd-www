47e14338a4f17e18e099c1c7658fd0a411f86485
Andy White <greatwhitehope@gmail.com>
2012-11-20 16:51:41 -0700

    Basic minify build working, r.js still not working quite like I want (not concatenating all the require modules)

--- build/build.require.js
+++ build/build.require.js
@@ -1,10 +1,8 @@
 // See https://github.com/jrburke/r.js/blob/master/build/example.build.js
 // All paths are relative to the location of this file
 ({
-    baseUrl: "../",
-
-    dir: "../intermediate",
-
+    baseUrl: "../intermediate",
+    dir: "../publish",
     modules: [
         {
             name: "config"
