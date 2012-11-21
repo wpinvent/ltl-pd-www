8c15501b10db6fb66313811f0c98162825ab760c
Andy White <greatwhitehope@gmail.com>
2012-11-20 17:18:00 -0700

    Re-organized config/main to get r.js to work properly - note: it only works correctly with node, not when run in Rhino

--- build/build.require.js
+++ build/build.require.js
@@ -3,10 +3,38 @@
 ({
     baseUrl: "../intermediate",
     dir: "../publish",
+
+    // TODO: Copied the paths in from main.js... not sure why r.js can't get the paths
+    // from the main.js file...
+    paths: {
+        // Require.js plugins
+        text: 'lib/require-2.1.1/text',
+        domReady: 'lib/require-2.1.1/domReady',
+        i18n: 'lib/require-2.1.1/i18n',
+        tpl: 'lib/require-2.1.1/tpl',
+
+        // jQuery
+        jquery: 'lib/jquery-1.8.2/jquery',
+
+        // Underscore
+        underscore: 'lib/underscore-1.4.2/underscore',
+
+        // Backbone
+        backbone: 'lib/backbone-0.9.2/backbone',
+
+        // Backbone.Marionette
+        // Has AMD support built-in (doesn't need to be shimmed)
+        marionette: 'lib/backbone.marionette-1.0.0-beta5-AMD/backbone.marionette',
+
+        // Twitter Bootstrap JavaScript
+        bootstrap: 'lib/twitter-bootstrap-2.2.1/js/bootstrap'
+    },
+
     modules: [
         {
-            name: "config"
+            name: "main"
         }
-    ]
+    ],
+
 
 })
---
+++ main.js
@@ -0,0 +1,103 @@
+//(function(console, require) {
+
+    console.log('Entering main');
+
+    /**
+     * Sets up require.js, and defines the main entry point into the application (js/main)
+     */
+    require.config({
+
+        // Paths to common dependencies
+        paths: {
+            // Require.js plugins
+            text: 'lib/require-2.1.1/text',
+            domReady: 'lib/require-2.1.1/domReady',
+            i18n: 'lib/require-2.1.1/i18n',
+            tpl: 'lib/require-2.1.1/tpl',
+
+            // jQuery
+            jquery: 'lib/jquery-1.8.2/jquery',
+
+            // Underscore
+            underscore: 'lib/underscore-1.4.2/underscore',
+
+            // Backbone
+            backbone: 'lib/backbone-0.9.2/backbone',
+
+            // Backbone.Marionette
+            // Has AMD support built-in (doesn't need to be shimmed)
+            marionette: 'lib/backbone.marionette-1.0.0-beta5-AMD/backbone.marionette',
+
+            // Twitter Bootstrap JavaScript
+            bootstrap: 'lib/twitter-bootstrap-2.2.1/js/bootstrap'
+        },
+
+        // Shim in dependencies that are not AMD-compliant by default
+        shim: {
+            jquery: {
+                exports: '$'
+            },
+
+            underscore: {
+                exports: '_'
+            },
+
+            backbone: {
+                deps: ['underscore', 'jquery'],
+                exports: 'Backbone'
+            },
+
+            bootstrap: {
+                deps: ['jquery'],
+                exports: 'bootstrap'
+            }
+        }
+    });
+
+    /**
+     * Main entry point into the application
+     */
+    require(['domReady', 'jquery', 'underscore', 'backbone', 'js/app', 'js/routers/appRouter', 'bootstrap'],
+        function (domReady, $, _, Backbone, app, appRouter) {
+
+            console.log('Entering js/main');
+
+            // Wait for DOM ready...
+            domReady(function () {
+
+                console.log('DOM ready!');
+
+                // Function to run when Cordova is ready
+                var onDeviceReady = function(isDesktop) {
+
+                    console.log('Device ready!');
+
+                    if (isDesktop !== true) {
+                        cordova.exec(null, null, 'SplashScreen', 'hide', []);
+                    }
+
+                    app.start({
+                        isDesktop: isDesktop,
+                        router: appRouter
+                    });
+
+                    // Hide the static loading div
+                    $('#loading').slideUp(600);
+                };
+
+                // Setup listener for Cordova deviceready...
+                if (navigator.userAgent.match(/(iPad|iPhone|Android)/)) {
+                    // Device - must wait for deviceready
+                    document.addEventListener('deviceready', function() {
+                        onDeviceReady(false)
+                    }, false);
+                } else {
+                    // Desktop - deviceready is N/A, just apply a simulated loading delay
+                    setTimeout(function() {
+                        onDeviceReady(true);
+                    }, 500);
+                }
+            });
+        }
+    );
+//}(console, require));
