206cd00095aca7211520ea667046a4b353a76d1f
Andy White <greatwhitehope@gmail.com>
2012-12-01 21:11:04 -0700

    Add grunt requirejs plugin

--- grunt.js
+++ grunt.js
@@ -3,20 +3,11 @@
 module.exports = function(grunt) {
     'use strict';
 
-    ////////////////////////////////////////////////////////////////////////////////
-    // Grunt configuration options
-    ////////////////////////////////////////////////////////////////////////////////
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
     grunt.initConfig({
 
-        ////////////////////////////////////////////////////////////////////////////////
-        // Loads the npm package.json file into the pkg variable for use below
-        ////////////////////////////////////////////////////////////////////////////////
-
         pkg: '<json:package.json>',
-
-        ////////////////////////////////////////////////////////////////////////////////
-        // Defines meta information for the build
-        ////////////////////////////////////////////////////////////////////////////////
 
         meta: {
             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
@@ -26,10 +17,6 @@ module.exports = function(grunt) {
                 ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
         },
 
-        ////////////////////////////////////////////////////////////////////////////////
-        // Files to check with JSHint
-        ////////////////////////////////////////////////////////////////////////////////
-
         lint: {
             grunt: 'grunt.js',
             main: 'src/main.js',
@@ -38,26 +25,59 @@ module.exports = function(grunt) {
             test: 'test/**/*.js'
         },
 
-        ////////////////////////////////////////////////////////////////////////////////
-        // Files to test using qunit
-        ////////////////////////////////////////////////////////////////////////////////
-
         qunit: {
             files: ['test/**/*.html']
         },
 
-        ////////////////////////////////////////////////////////////////////////////////
-        // Watch files
-        ////////////////////////////////////////////////////////////////////////////////
+        requirejs: {
+            compile: {
+                options: {
+                    // See https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                    baseUrl: "src",
+                    dir: "dist",
+                    paths: {
+                        // Require.js plugins
+                        text: 'lib/require-2.1.1/text',
+                        domReady: 'lib/require-2.1.1/domReady',
+                        i18n: 'lib/require-2.1.1/i18n',
+                        tpl: 'lib/require-2.1.1/tpl',
+
+                        // jQuery
+                        jquery: 'lib/jquery-1.8.2/jquery',
+
+                        // Underscore
+                        //underscore: 'lib/underscore-1.4.2/underscore',
+                        underscore: 'lib/lodash-0.10.0/lodash',
+
+                        // Backbone
+                        //backbone: 'lib/backbone-0.9.2/backbone',
+                        backbone: 'lib/backbone-0.9.2/backbone-AMD',
+
+                        // Backbone.Marionette
+                        // Has AMD support built-in (doesn't need to be shimmed)
+                        marionette: 'lib/backbone.marionette-1.0.0-beta5-AMD/backbone.marionette',
+                        marionetteAsync: 'lib/backbone.marionette.async-0.2.0-AMD/backbone.marionette.async',
+
+                        // Backbone.BackStack
+                        backstack: 'lib/backstack-1.1.2/backstack',
+
+                        // Twitter Bootstrap JavaScript
+                        bootstrap: 'lib/twitter-bootstrap-2.2.1/js/bootstrap'
+                    },
+
+                    modules: [
+                        {
+                            name: "main"
+                        }
+                    ]
+                }
+            }
+        }
 
         watch: {
             files: '<config:lint.files>',
             tasks: 'lint qunit'
         },
-
-        ////////////////////////////////////////////////////////////////////////////////
-        // JSHint options
-        ////////////////////////////////////////////////////////////////////////////////
 
         jshint: {
             options: {
@@ -144,5 +164,5 @@ module.exports = function(grunt) {
     });
 
     // Default task.
-    grunt.registerTask('default', 'lint qunit');
+    grunt.registerTask('default', 'lint qunit requirejs');
 };
